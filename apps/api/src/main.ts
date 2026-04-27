374cc1e9dec5a064be4681ace50230678e5a4cda
Muhamad Irsal <muhamadirsal@gmail.com>
2026-04-28 04:11:19 +0700

    refactor: update cookie-parser import to use default export

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as fs from 'fs';
 import * as path from 'path';
 import { ValidationPipe } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
