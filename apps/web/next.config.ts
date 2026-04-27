a8c38b52e349b8e063264a349af2561bf1940583
Muhamad Irsal <muhamadirsal@gmail.com>
2026-04-28 01:44:01 +0700

    refactor: remove Dockerfile and disable standalone mode in next config to simplify deployment workflow

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,10 +1,7 @@
-import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   transpilePackages: ["@repo/ui"],
-  outputFileTracingRoot: path.join(__dirname, "../../"),
   async rewrites() {
     return [
       {
